dc5a368882236a7e231c30f87a45af75d63be839
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-10-17 06:39:57 +0200

    First commit

---
+++ services/wordpress/wp-content/themes/docker/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Docker Theme</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
+        <?php wp_head() ?>
+    </head>
+    <body class="container">
+
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>
+                    <?php echo get_bloginfo( 'name' ); ?><br>
+                    <small><?php echo get_bloginfo( 'description' ); ?></small>
+                </h1>
+            </div>
+        </div>
+        <hr>
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="row">
+                <div class="col-xs-12">
+                    <h4><?php the_title(); ?></h4>
+				    <?php the_content(); ?>
+                </div>
+            </div>
+            <hr>
+		<?php endwhile; endif;?>
+
+        <?php wp_footer() ?>
+    </body>
+</html>
