81e5e091a238e4199e7055869848d777b5b75320
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-12-12 12:04:04 +0100

    #37 list post-format video with a custom youtube icon

--- services/wordpress/wp-content/themes/docker/post-list/post-format-video.php
+++ services/wordpress/wp-content/themes/docker/post-list/post-format-video.php
@@ -1,7 +1,3 @@
-<?php
-$gallery_ids = post_gallery_lite_unserialize($post->ID);
-$gallery_images = post_gallery_lite_fetch_images($gallery_ids);
-?>
 <div class="post-list-item">
     <div class="container-fluid">
         <div class="row">
@@ -12,17 +8,23 @@ $gallery_images = post_gallery_lite_fetch_images($gallery_ids);
                     <span class="post-date__year"><?php the_time('Y') ?></span>
                 </time>
             </div>
-            <div class="col-xs-10">
-                <h2 class="post-list-item__headline post-list-item__headline--nomargin">
+            <div class="col-xs-8">
+                <div class="post-list-item__author">
+                    <?php the_author_full_name() ?>
+                </div>
+                <h2 class="post-list-item__headline">
                     <a href="<?php echo esc_url( get_permalink() )?>" title="<?php the_title(); ?>">
                         <?php the_title(); ?>
                     </a>
                 </h2>
                 <div class="post-list-item__content">
-                    <?php foreach ( $gallery_images as $image ) : ?>
-                        <img src="<?php echo $image['thumb'] ?>" width="40" height="40" />
-                    <?php endforeach; ?>
+                    <?php is_single() ? the_content() : the_excerpt() ?>
                 </div>
+            </div>
+            <div class="col-xs-2">
+                <a href="<?php echo esc_url( get_permalink() )?>" title="<?php the_title(); ?>">
+                    <img src="<?php echo get_template_directory_uri() . '/images/youtube.png' ?>" alt="YouTUBE Video" class="img-responsive" />
+                </a>
             </div>
         </div>
     </div>
