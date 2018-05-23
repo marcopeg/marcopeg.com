bf3d6bd1a1169be339c3ae833a91ce77d1dcaf3e
lenguyenitc <lenguyenitc@gmail.com>
2018-05-23 11:45:21 +0700

    #48 add posts pagination in the home page

--- services/wordpress/wp-content/themes/docker/index.php
+++ services/wordpress/wp-content/themes/docker/index.php
@@ -23,6 +23,21 @@
             }
             get_template_part('post-list/post-format', $format);
         }
+        // add paging
+        global $wp_query;
+        if( $wp_query->max_num_pages > 1 ): ?>
+        <div class="docker_paginate_links">
+        <?php
+          $big = 999999999; // need an unlikely integer
+          echo paginate_links( array(
+          	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+          	'format' => '?paged=%#%',
+          	'current' => max( 1, get_query_var('paged') ),
+          	'total' => $wp_query->max_num_pages,
+          ) ); ?>
+        </div>
+        <?php
+        endif;
     } ?>
 
 <?php get_footer();
