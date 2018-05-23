88f814d0ee1e5c923832b22d6a088fc38fe62990
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-05-23 10:12:52 +0200

    Remove state files due to fuckup with .gitignore

--- data/production/wp-content/plugins/mpcx-lightbox/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
-}
-
-delete_option( 'mpcx_lightbox' );
--- data/production/wp-content/plugins/redirection/actions/nothing.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Nothing_Action extends Red_Action {
-	public function process_before( $code, $target ) {
-		return apply_filters( 'redirection_do_nothing', false, $target );
-	}
-}
